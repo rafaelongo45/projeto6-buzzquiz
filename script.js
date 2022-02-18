d33328bb7c744ce283fa93811754c02660152682
Henrique Butzke Kotz <henriquebkotz@gmail.com>
2022-02-18 15:59:42 -0300

    criação do quizz: perguntas do quizz

--- script.js
+++ script.js
@@ -204,7 +204,9 @@ function clicaResposta(elemento){
 }
 
 
-function fillQuizzInfo() {
+function fillQuizzInfo(event) { //use input IDs
+    event.preventDefault();
+    createdQuizz = {};
     const inputs = document.querySelectorAll('.criacao-info form input');
     const inputValues = [...inputs].map(input => input.value);
     
@@ -214,24 +216,132 @@ function fillQuizzInfo() {
         numQuestions: parseInt(inputValues[2]),
         numLevels: parseInt(inputValues[3])
     };
-    console.log(quizzInfo);
     
     const validTitle = quizzInfo.title.length >= 20 && quizzInfo.title.length <= 65;
     const validImage = validateURL(quizzInfo.image);
     const validQuestions = quizzInfo.numQuestions >= 3 && Number.isInteger(quizzInfo.numQuestions);
     const validLevels = quizzInfo.numLevels >= 2 && Number.isInteger(quizzInfo.numLevels);
-    console.log(validTitle);
 
     if (validTitle && validImage && validQuestions && validLevels) {
         document.querySelector('.criacao-info').classList.toggle('escondido');
         document.querySelector('.criacao-perguntas').classList.toggle('escondido');
+        displayQuizzQuestions();
+
+        createdQuizz.title = quizzInfo.title;
+        createdQuizz.image = quizzInfo.image;
     } else {
         alert('Preencha os dados corretamente');
     }
 }
 
+function displayQuizzQuestions() {
+    const questionList = document.querySelector('.criacao-perguntas ul');
+    for (let i=0; i<quizzInfo.numQuestions; i++) {
+        questionList.innerHTML += `
+        <li>
+            <div class="pergunta-form" onclick="toggleQuestion(this)">
+                <h2>Pergunta ${i+1}</h2>
+                <ion-icon name="create-outline"></ion-icon>
+            </div>
+
+            <div class="pergunta-info escondido">
+                <input type="text" minlength="20" name="texto-pergunta" id="texto-pergunta" placeholder="Texto da pergunta" required value="titulo da pergunta maneira">
+                <input type="text" pattern="#[0-9a-fA-F]{6}" name="cor-pergunta" id="cor-pergunta" placeholder="Cor de fundo da pergunta" required value="#123456">
+                
+                <label>Resposta correta</label>
+                <input type="text" name="resposta-correta" id="resposta-correta" placeholder="Resposta correta" required value="resposta correta">
+                <input type="url" name="url-correta" id="url-correta" placeholder="URL da imagem" required value="https://gofrag.ru/images/73/gas_station_simulator-1.jpg">
+                
+                <label>Respostas incorretas</label>
+                <input type="text" name="resposta-incorreta" id="resposta-incorreta" placeholder="Resposta incorreta 1" required value="resposta incorreta po">
+                <input type="url" name="url-incorreta" id="url-incorreta" placeholder="URL da imagem 1" required value="https://gofrag.ru/images/73/gas_station_simulator-1.jpg">
+                <input type="text" name="resposta-incorreta" id="resposta-incorreta" placeholder="Resposta incorreta 2">
+                <input type="url" name="url-incorreta" id="url-incorreta" placeholder="URL da imagem 2">
+                <input type="text" name="resposta-incorreta" id="resposta-incorreta" placeholder="Resposta incorreta 3">
+                <input type="url" name="url-incorreta" id="url-incorreta" placeholder="URL da imagem 3">
+            </div>
+        </li>
+        `;
+    }
+}
+
+function fillQuizzQuestions(event) {  /* too many loops? */
+    console.log(createdQuizz);
+    event.preventDefault();
+    const questionNodes = document.querySelectorAll('.criacao-perguntas .pergunta-info');
+
+    const questionsList = [];
+    for (let i=0; i<quizzInfo.numQuestions; i++) {
+        const inputs = questionNodes[0].querySelectorAll('input');
+        const inputList = [...inputs].filter(input => input.value);
+
+        const question = {
+            title: inputList.find(input => input.id === 'texto-pergunta').value,
+            color: inputList.find(input => input.id === 'cor-pergunta').value,
+            answers: [
+                {
+                    text: inputList.find(input => input.id === 'resposta-correta').value,
+                    image: inputList.find(input => input.id === 'url-correta').value,
+                    isCorrectAnswer: true
+                }
+            ]
+        };
+
+        const wrongAnswers = inputList.filter(input => input.id === 'resposta-incorreta');
+        const wrongURLs = inputList.filter(input => input.id === 'url-incorreta');
+        for (let j=0; j<wrongAnswers.length; j++) {
+            const wrongAnswer = {
+                text: wrongAnswers[j].value,
+                image: wrongURLs[j].value,
+                isCorrectAnswer: false
+            }
+            question.answers.push(wrongAnswer);
+        }
+
+        questionsList.push(question);
+    }
+
+    if(validateQuestionInputs(questionsList)) {
+        createdQuizz.questions = questionsList;
+        document.querySelector('.criacao-perguntas').classList.toggle('escondido');
+        document.querySelector('.criacao-niveis').classList.toggle('escondido');
+
+        console.log(createdQuizz);
+    } else {
+        alert('Preencha os dados corretamente');
+    }
+
+}
+
+function validateQuestionInputs(questionsList) {
+    console.log(questionsList);
+    const regexColor = /^#[0-9a-fA-F]{6}$/;
+    
+    for (let i=0; i<questionsList.length; i++) {
+        const question = questionsList[i];
+        const validTitle = question.title.length >= 20;
+        const validColor = regexColor.test(question.color);
+
+        if(!(validTitle && validColor)) return false;
+
+        const numAnswers = question.answers.length;
+        for (let j=0; j<numAnswers; j++) {
+            const answer = question.answers[j];
+            const validText = answer.text !== '';
+            
+            if (!(validText && validateURL(answer.image))) return false;
+        }
+    }
+    return true;
+}
+
+function toggleQuestion(elem) {
+    const questionsInfo = elem.nextElementSibling;
+    questionsInfo.classList.toggle('escondido');
+    elem.querySelector('ion-icon').classList.toggle('escondido');
+}
+
 function validateURL(string) {
-    //url needs to have 'http(s)' or 'www.', and '.(file extension)'
     const regexURL = /^(http(s)?:\/\/)|(www\.)/;
     const regexImage = /\.(jpeg|jpg|png|gif|svg)$/;
     const isValid = regexURL.test(string) && regexImage.test(string);
